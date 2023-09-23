18c7722daa514f2c13ba9ff08c7cd745c7078909
Yasir Khan <mohdyasirkhan091@gmail.com>
2023-09-23 15:58:30 +0530

    Created Heading of RightSectionInChatPage Component and added CSS to it

--- frontend/src/components/RightSectionInChatPage.jsx
+++ frontend/src/components/RightSectionInChatPage.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import './RightSectionInChatPage.css'
+import VideocamIcon from '@mui/icons-material/Videocam';
+import CallIcon from '@mui/icons-material/Call';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 export default function RightSectionInChatPage() {
-  return (
-    <div>RightSectionInChatPage</div>
-  )
+    return (
+        <>
+            <div className={`chatRightSection`}>
+                <div className="chatHeadingOfRightSection d-flex align-items-center justify-content-between">
+                    <div className='displayFlexContainer'>
+                        <div className="imageInMessage me-2">
+                            <img src={"/images/BotImage1.png"} alt="Profile" />
+                        </div>
+                        <span>CoalBot</span>
+                    </div>
+                    <div className="chatListOfIconsInRightSectionHeading displayFlexContaier">
+                        <VideocamIcon className='chatIconsInRightSectionHeading' />
+                        <CallIcon className='chatIconsInRightSectionHeading' />
+                        <MoreVertIcon className='chatIconsInRightSectionHeading' />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
