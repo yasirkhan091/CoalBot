3db2f470d4cce7606a0d920bf950a07d6689e0dd
Yasir Khan <mohdyasirkhan091@gmail.com>
2023-09-23 15:48:32 +0530

    Created the HTML structure of the ChatPage component and gave CSS to it

--- frontend/src/components/ChatPage.jsx
+++ frontend/src/components/ChatPage.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import "./ChatPage.css"
+import { RightSectionInChatPage } from './RightSectionInChatPage';
 
-export default function ChatPage() {
+
+export const ChatPage = () => {
+
   return (
-    <div>ChatPage</div>
+    <div className="container-fluid p-0">
+    <div className='chatOuterContainer w-100 displayFlexContainer row m-0'>
+      <div className="chatInnerContainer d-flex col-10 col-md-8 col-lg-6 mx-auto p-0">
+  
+        <RightSectionInChatPage/>
+        
+      </div>
+    </div>
+    </div>
   )
 }
